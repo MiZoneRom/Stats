1bff80d154fa0c648b9705eb48a5e458eae089d3
MiZone <mizonerom@hotmail.com>
2026-08-24 22:13:27 +0800

    Initial Commit

---
+++ stats/statsApp.swift
@@ -0,0 +1,17 @@
+//
+//  statsApp.swift
+//  stats
+//
+//  Created by 刘海威 on 2026/8/24.
+//
+
+import SwiftUI
+
+@main
+struct statsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
